1f20a99d3838a7f9ab54ad641591fcd82e68f5c9
Peter Siemens <peterjsiemens@gmail.com>
2022-03-19 13:44:47 -0700

    Add sample JSON objects

--- src/AccountSignature.php
+++ src/AccountSignature.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+/*
+Sample JSON:
+
+{
+    "f_type": "CompositeSignature",
+    "f_vsn": "1.0.0",
+    "addr": "0xe7b6c0eb222b9f6f",
+    "keyId": 0,
+    "signature": "64875393dea7247574d67008f93caf9b309dc270a48eb36c03ef5f5ef373bde4cc59b2b627764bdb22f3b30fee341061b3dcff3ccb69b26b7094874f52679ac5"
+}
+
+*/
 final class AccountSignature
 {
     public $address;
--- src/FCLClient.php
+++ src/FCLClient.php
@@ -31,11 +31,42 @@ final class FCLClient extends HTTPClient
     public function authn_poll(string $endpoint, array $params): AuthnPollResponse
     {
         $response = $this->call("GET", $endpoint, $params);
-
+        
         return AuthnPollResponse::from_json($response);
     }
 }
 
+/*
+Sample JSON:
+
+{
+    "f_type": "PollingResponse",
+    "f_vsn": "1.0.0",
+    "status": "PENDING",
+    "reason": "",
+    "updates": {
+        "f_type": "Service",
+        "f_vsn": "1.0.0",
+        "type": "back-channel-rpc",
+        "endpoint": "https://localhost:8000/api/fcl/authn",
+        "method": "HTTP/GET",
+        "params": {
+            "authnID": "961cfa4e-0105-4337-9ea0-cc4fc9a5f4b0"
+        }
+    },
+    "local": {
+        "f_type": "Service",
+        "f_vsn": "1.0.0",
+        "type": "local-view",
+        "endpoint": "https://localhost:8000/fcl/authn",
+        "method": "VIEW/POP",
+        "params": {
+            "authnID": "961cfa4e-0105-4337-9ea0-cc4fc9a5f4b0",
+        }
+    }
+}
+
+*/
 final class AuthnResponse
 {
     public $updates;
@@ -58,6 +89,21 @@ final class AuthnResponse
     }
 }
 
+/*
+Sample JSON:
+
+{
+    "f_type": "Service",
+    "f_vsn": "1.0.0",
+    "type": "back-channel-rpc",
+    "endpoint": "https://localhost:8000/api/fcl/authn",
+    "method": "HTTP/GET",
+    "params": {
+        "authnID": "961cfa4e-0105-4337-9ea0-cc4fc9a5f4b0"
+    }
+}
+
+*/
 final class AuthnUpdates
 {
     public $endpoint;
@@ -80,6 +126,21 @@ final class AuthnUpdates
     }
 }
 
+/*
+Sample JSON:
+
+{
+    "f_type": "Service",
+    "f_vsn": "1.0.0",
+    "type": "local-view",
+    "endpoint": "https://localhost:8000/fcl/authn",
+    "method": "VIEW/POP",
+    "params": {
+        "authnID": "961cfa4e-0105-4337-9ea0-cc4fc9a5f4b0",
+    }
+}
+
+*/
 final class AuthnLocal
 {
     public $endpoint;
@@ -111,6 +172,103 @@ final class AuthnLocal
     }
 }
 
+/*
+Sample JSON (pending):
+
+{
+    "f_type": "PollingResponse",
+    "f_vsn": "1.0.0",
+    "status": "PENDING",
+    "reason": "",
+    "updates": {
+        "f_type": "Service",
+        "f_vsn": "1.0.0",
+        "type": "back-channel-rpc",
+        "endpoint": "https://localhost:8000/api/fcl/authn",
+        "method": "HTTP/GET",
+        "params": {
+            "authnID": "961cfa4e-0105-4337-9ea0-cc4fc9a5f4b"
+        }
+    }
+}
+
+Sample JSON (declined):
+
+{
+    "f_type": "PollingResponse",
+    "f_vsn": "1.0.0",
+    "status": "DECLINED",
+    "reason": "",
+    "updates": {
+        "f_type": "Service",
+        "f_vsn": "1.0.0",
+        "type": "back-channel-rpc",
+        "endpoint": "https://localhost:8000/api/fcl/authn",
+        "method": "HTTP/GET",
+        "params": {
+            "authnID": "961cfa4e-0105-4337-9ea0-cc4fc9a5f4b"
+        }
+    }
+}
+
+Sample JSON (approved):
+
+{
+    "f_type": "PollingResponse",
+    "f_vsn": "1.0.0",
+    "status": "APPROVED",
+    "reason": "",
+    "data": {
+        "f_type": "AuthnResponse",
+        "f_vsn": "1.0.0",
+        "addr": "0xe7b6c0eb222b9f6f",
+        "services": [
+            {
+                "f_type": "Service",
+                "f_vsn": "1.0.0",
+                "type": "authn",
+                "method": "DATA",
+                "uid": "example-wallet#authn",
+                "endpoint": "https://localhost:8000/fcl/authn",
+                "id": "0xe7b6c0eb222b9f6f",
+                "identity": {
+                    "f_type": "Identity",
+                    "f_vsn": "1.0.0",
+                    "address": "0xe7b6c0eb222b9f6f"
+                },
+                "provider": {
+                    "f_type": "ServiceProvider",
+                    "f_vsn": "1.0.0",
+                    "address": "0x82ec283f88a62e65"
+                }
+            },
+            {
+                "f_type": "Service",
+                "f_vsn": "1.0.0",
+                "type": "account-proof",
+                "uid": "example-wallet#account-proof",
+                "method": "DATA",
+                "data": {
+                    "f_type": "Service",
+                    "f_vsn": "1.0.0",
+                    "signatures": [
+                        {
+                            "f_type": "CompositeSignature",
+                            "f_vsn": "1.0.0",
+                            "addr": "0xe7b6c0eb222b9f6f",
+                            "keyId": 0,
+                            "signature": "64875393dea7247574d67008f93caf9b309dc270a48eb36c03ef5f5ef373bde4cc59b2b627764bdb22f3b30fee341061b3dcff3ccb69b26b7094874f52679ac5"
+                        }
+                    ],
+                    "address": "0xe7b6c0eb222b9f6f",
+                    "nonce": "75f8587e5bd5f9dcc9909d0dae1f0ac5814458b2ae129620502cb936fde7120a"
+                }
+            }
+        ]
+    }
+}
+
+*/
 final class AuthnPollResponse
 {
     public $status;
